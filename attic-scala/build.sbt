2a34806b3ba581915cbc13daa0e2ef65f21cddbd
xnnyygn <xnnyygn@gmail.com>
2014-02-25 14:28:08 +0800

    remove attic-scala

--- attic-scala/build.sbt
+++
@@ -1,5 +0,0 @@
-name := "attic"
-
-version := "1.0.0"
-
-libraryDependencies += "org.seleniumhq.selenium" % "selenium-java" % "2.35.0"
--- attic-scala/src/main/scala/ThirdPartyCommands.scala
+++
@@ -1,7 +0,0 @@
-package in.xnnyygn.attic.thirdparty.command
-
-import in.xnnyygn.attic.core.{Command, CommandContext, NullaryCommand}
-
-class PrintHelloWorldCommand extends NullaryCommand {
-  def execute(context: CommandContext) = println("Hello, world!")
-}
